4979a51ac824e33b27f592ad3d177502d09b5d03
Selariu Alexandru <Selalex_gd@yahoo.com>
2018-10-04 16:58:02 +0300

    filter cars

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
 window.addEventListener("load", () =>
   document.querySelector(".preloader").classList.add("hidePreloader")
 );
+
+// filter cars
+
+const filter = document.querySelectorAll(".filter-btn");
+
+filter.forEach(btn => {
+  btn.addEventListener("click", event => {
+    const value = event.target.dataset.filter;
+    console.log(value);
+
+    const singleCar = document.querySelectorAll(".single-car");
+
+    singleCar.forEach(car => {
+      if (value === "all") {
+        car.style.display = "block";
+      } else {
+        !car.classList.contains(value)
+          ? (car.style.display = "none")
+          : (car.style.display = "block");
+      }
+    });
+  });
+});
